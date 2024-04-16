3db0068493a184b2aa9787dcceac697806bad617
kj139 <therealkjai139@gmail.com>
2024-04-15 21:46:51 -0400

    test endpoint

--- frontend/src/app/_utils/apiEndpoint.js
+++ frontend/src/app/_utils/apiEndpoint.js
@@ -1,5 +1,5 @@
 
-const apiUrl = process.env.NODE_ENV === 'production' ? '' : process.env.NEXT_PUBLIC_API_LOCAL_URL
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_PROD_URL : process.env.NEXT_PUBLIC_API_LOCAL_URL
 
 
 export default apiUrl
