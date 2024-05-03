3a461f51c657697f606a71b0274090431f51c176
kj139 <therealkjai139@gmail.com>
2024-05-02 23:10:21 -0400

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -22,12 +22,15 @@ const cspHeader = `
 
 const nextConfig = {
     images: {
-        /* deviceSizes: [320, 420, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes:[16, 32, 48, 64, 96, 128, 256, 310], */
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_S3_URL,
+                hostname: process.env.NEXT_PUBLIC_CLOUDFRONT,
+                port: ''
+            },
+            {
+                protocol: 'https',
+                hostname:  process.env.NEXT_PUBLIC_S3_URL,
                 port: ''
             }
         ]
