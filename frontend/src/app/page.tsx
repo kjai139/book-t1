6fa20975a98452d05657bd1fc492846ad2db1acf
kj139 <therealkjai139@gmail.com>
2024-05-10 17:24:05 -0400

    img optimization

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -176,7 +176,7 @@ export default async function Home() {
             <div className="relative w-full min-h-[200px] overflow-hidden">
               
              
-              <NextImage src={node.book.image} alt={`Cover image of ${node.book.name}`} priority={idx <= 1 ? true : false} fill sizes="(max-width:420px) 50vw,(max-width:600px) 33vw, (max-width:1200px) 10vw, 10vw" className="home-c object-cover rounded">
+              <NextImage src={node.book.image} alt={`Cover image of ${node.book.name}`} priority={idx <= 1 ? true : false} fill sizes="(max-width:420px) 50vw,(max-width:600px) 33vw, (max-width:800px) 25vw, (max-width:1060px) 20vw, 10vw" className="home-c object-cover rounded">
 
               </NextImage>
               <span className="flex absolute top-1 left-1 gap-1 items-center">
