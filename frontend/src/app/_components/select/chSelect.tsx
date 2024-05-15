bb92e8597f01e29dd47d9ed88237167a202979a4
kj139 <therealkjai139@gmail.com>
2024-05-15 14:43:43 -0400

    fix chapterselect

--- frontend/src/app/_components/select/chSelect.tsx
+++ frontend/src/app/_components/select/chSelect.tsx
@@ -10,18 +10,20 @@ interface ChSelectProps {
 
 export default function ChSelect ({chList, curCh, wtName}:ChSelectProps) {
 
+   
     const disabledKeys = chList.filter(ch => ch.chapterNumber === Number(curCh)).map(ch => ch._id)
-    /* console.log(disabledKeys) */
+    
     return (
         <Select
         items={chList}
         label="Select a chapter"
         size="sm"
+        defaultSelectedKeys={disabledKeys}
         disabledKeys={disabledKeys}
         placeholder={`Chapter ${curCh}`}
         className="max-w-xs"
         >
-            {(ch) => <SelectItem href={`/read/${wtName}/${ch.chapterNumber.toString()}`} key={ch._id}>{`Chapter ${ch.chapterNumber}`}</SelectItem>}
+            {(ch) => <SelectItem href={`/read/${wtName}/${ch.chapterNumber}`} key={ch._id}>{`Chapter ${ch.chapterNumber}`}</SelectItem>}
 
         </Select>
     )
