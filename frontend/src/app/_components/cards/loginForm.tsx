362abb5e354fe4a3dc9a8c275ed6190bf3e7beb1
kj139 <therealkjai139@gmail.com>
2024-06-26 00:22:32 -0400

    start auth

--- frontend/src/app/_components/cards/loginForm.tsx
+++ frontend/src/app/_components/cards/loginForm.tsx
@@ -8,6 +8,7 @@ import ResultModal from '../../_components/modals/resultModal'
 import { Button } from '@nextui-org/react'
 import { IoIosAlert } from 'react-icons/io'
 import { useAuth } from '@/app/_contexts/authContext'
+import { FcGoogle } from 'react-icons/fc'
 
 const schema = yup.object({
     username: yup.string().required('A username is required').max(15),
@@ -81,41 +82,49 @@ export default function LoginForm () {
 
     return (
         <div className='h-screen items-center justify-center flex p-4'>
-        <form onSubmit={handleSubmit(onSubmit)} className={`flex flex-col p-2 gap-4 rounded shadow relative border-divider border-2 bg-content1`}>
-            {/* {isLoading &&
-            <span className='overlay-t'></span>
-            } */}
-            <span className='text-center text-lg font-bold'>
-                SIGN IN
-            </span>
-            <div>
-            <label htmlFor='username'>Username</label>
-            <input {...register('username')} id='username' autoComplete='off' maxLength={15} className='bg-default w-full rounded p-1'></input>
-            <p className='text-warning'>{errors.username?.message}</p>
-            </div>
-            
-            <div>
-            <label htmlFor='password'>Password</label>
-            <input {...register('password')} type='password' maxLength={20} autoComplete='off' id='password' className='bg-default w-full rounded p-1'></input>
-            <p className='text-warning'>{errors.password?.message}</p>
-            </div>
-            
-            
-            {/* <button type='submit' className={`${styles.submitBtn} p-1 bg-primary rounded`}>LOG IN</button> */}
-            <div>
-            <Button color='primary' isLoading={isLoading} radius='md' type='submit' fullWidth>
-                {isLoading ? 'Loading' : 'Log in'}
-            </Button>
-            </div>
-            <div className='flex gap-2 items-center'>
-                <IoIosAlert size={22}></IoIosAlert>
-                <span className='text-xs text-default-500'>
-                    Signups are not open at this moment.
+            <div className={`flex flex-col p-2 gap-4 rounded shadow relative border-divider border-2 bg-content1`}>
+            <form onSubmit={handleSubmit(onSubmit)} className='gap-4 flex flex-col'>
+                {/* {isLoading &&
+                <span className='overlay-t'></span>
+                } */}
+                <span className='text-center text-lg font-bold'>
+                    SIGN IN
                 </span>
-            </div>
+                <div>
+                <label htmlFor='username'>Username</label>
+                <input {...register('username')} id='username' autoComplete='off' maxLength={15} className='bg-default w-full rounded p-1'></input>
+                <p className='text-warning'>{errors.username?.message}</p>
+                </div>
+                
+                <div>
+                <label htmlFor='password'>Password</label>
+                <input {...register('password')} type='password' maxLength={20} autoComplete='off' id='password' className='bg-default w-full rounded p-1'></input>
+                <p className='text-warning'>{errors.password?.message}</p>
+                </div>
+                <div className='flex gap-2 items-center'>
+                    <IoIosAlert size={22}></IoIosAlert>
+                    <span className='text-xs text-default-500'>
+                        Signups are not open at this moment.
+                    </span>
+                </div>
+                
+                {/* <button type='submit' className={`${styles.submitBtn} p-1 bg-primary rounded`}>LOG IN</button> */}
+                
+                <Button color='primary' isLoading={isLoading} radius='md' type='submit' fullWidth>
+                    {isLoading ? 'Loading' : 'Log in'}
+                </Button>
+            </form>
+                <Button startContent={
+                <FcGoogle></FcGoogle>
+            }>Log in with Google</Button>
+                </div>
+                <div>
+                
+                
             
-           
-        </form>
+            
+        </div>
+            
         {errorMsg && 
         <ResultModal isOpen={true} message={errorMsg} title='Error' handleClose={() => setErrorMsg('')} ></ResultModal>
         }
