390fdbd88c241b51ac0bb43e75a7616e20901c84
kj139 <therealkjai139@gmail.com>
2024-07-03 02:52:04 -0400

    avatar sign out add

--- frontend/src/app/_components/avatar/userAvatar.tsx
+++ frontend/src/app/_components/avatar/userAvatar.tsx
@@ -1,6 +1,8 @@
 
-import { Avatar } from "@nextui-org/react";
-import { useSession } from "next-auth/react";
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
+import { useSession, signOut } from "next-auth/react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function UserAvatar() {
     const session = useSession()
@@ -10,9 +12,39 @@ export default function UserAvatar() {
         return <Avatar isDisabled name=""></Avatar>
     }
 
-    return (
-        <Avatar src={session.data.user.image!} name={session.data.user.name!}>
+    const pathname = usePathname()
 
-        </Avatar>
+    const dropdownAction = (key:any) => {
+        if (pathname === '/dashboard') {
+            signOut({callbackUrl: '/login'})
+        } else {
+            signOut()
+        }
+    }
+
+
+
+    return (
+        <Dropdown>
+            <DropdownTrigger>
+                <Avatar as={"button"} src={session.data.user.image!} name={session.data.user.name!}>
+
+                </Avatar>
+
+            </DropdownTrigger>
+            <DropdownMenu aria-label="User menu actions" disabledKeys={["profile"]} onAction={(key) => dropdownAction(key)}>
+                <DropdownItem key={"profile"} textValue={`Signed in as ${session.data.user.email}`}>
+                    <p>Signed in as {session.data.user.email}</p>
+                </DropdownItem>
+                <DropdownItem key={"bookmarks"}>
+                    Your bookmarks
+                </DropdownItem>
+                <DropdownItem key={"signout"} color="danger">
+                    Log Out
+                </DropdownItem>
+
+            </DropdownMenu>
+       
+        </Dropdown>
     )
 }
