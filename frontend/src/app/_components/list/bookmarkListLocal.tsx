815db1b32572e23e56baa1cefb36db7d760be6d7
kj139 <therealkjai139@gmail.com>
2024-07-25 01:46:04 -0400

    update local bm

--- frontend/src/app/_components/list/bookmarkListLocal.tsx
+++ frontend/src/app/_components/list/bookmarkListLocal.tsx
@@ -1,19 +1,22 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useTransition } from "react"
 import ErrorMsgModal from "../modals/errorModal"
-import { Pagination } from "@nextui-org/react"
-
+import { Pagination, Button, Link } from "@nextui-org/react"
+import NextImage from "next/image"
+import { useAuth } from "@/app/_contexts/authContext"
+import StarsOnly from "../rating/starsDisplayOnly"
 
 export default function BookmarkListLocal () {
 
     const limit = 10
     
-
-    const [bookmarks, setBookmarks] = useState()
+    const { setCheckLocal } = useAuth()
+    const [bookmarks, setBookmarks] = useState<any[]>()
     const [errorMsg, setErrorMsg] = useState('')
-    const [totalPages, setTotalPages] = useState<number | null>()
+    const [totalPages, setTotalPages] = useState<number>(0)
     const [currentPage, setCurrentPage] = useState(1)
+    const [isPending, startTransition] = useTransition()
     
 
     useEffect(() => {
@@ -28,43 +31,53 @@ export default function BookmarkListLocal () {
         
     }, [])
 
+    const removeBm = (key:any) => {
+        let storedBookmarks = localStorage.getItem('bookmarks')
+        if (storedBookmarks) {
+            const parsedBookmarks:any[] = JSON.parse(storedBookmarks)
+            const existingBm = parsedBookmarks.findIndex(bm => bm.url === key)
+
+            if (existingBm !== -1) {
+                const updatedBm = parsedBookmarks.filter((_:any, index:any) => index !== existingBm)
+                localStorage.setItem('bookmarks', JSON.stringify(updatedBm))
+                console.log('bm removed:', localStorage)
+                setBookmarks(prevArr => prevArr!.filter(item => item.url !== key))
+                setCheckLocal((prev) => !prev)
+            }
+        }
+    }
+
     return (
         <>
         {
             errorMsg ?
             <ErrorMsgModal message={errorMsg} setErrorMsg={setErrorMsg}></ErrorMsgModal> : null
         }
-        {/* {
-            bookmarks ?
+        {
+            bookmarks && totalPages > 0 ?
             <>
             <Pagination className="my-4" total={totalPages} page={currentPage} showControls onChange={setCurrentPage}>
 
             </Pagination>
             <ul>
                 {bookmarks && bookmarks.map((bm:any, idx) => {
-        
+                    console.log('li keys', bm.url)
                     return (
-                        <li key={bm._id} className="mt-4">
+                        <li key={`bmlocal-${bm.url}`} className="mt-4">
                             <div className="flex">
                                 <div className="mr-4 flex-shrink-0">
-                                <NextImage priority={idx < 3 ? true : false} className="w-[100px] h-auto" unoptimized width={0} height={0} alt={`Cover image of ${bm.wtRef.name}`} src={bm.wtRef.image}></NextImage>
-                                <Button onPress={() => removeBm(bm._id)} isDisabled={isPending} radius="none" className="text-foreground mt-2 font-semibold" variant="ghost" color="warning" size="sm" fullWidth>Remove</Button>
+                                <NextImage priority={idx < 3 ? true : false} className="w-[100px] h-auto" unoptimized width={0} height={0} alt={`Cover image of ${bm.name}`} src={bm.image}></NextImage>
+                                <Button onPress={() => removeBm(bm.url)} isDisabled={isPending} radius="none" className="text-foreground mt-2 font-semibold" variant="ghost" color="warning" size="sm" fullWidth>Remove</Button>
                                 </div>
                                 <div className="flex flex-col">
                                     <Link isDisabled={isPending} href={bm.url} color="foreground" className="font-semibold">
-                                        {bm.wtRef.name}
+                                        {bm.name}
                                     </Link>
                                     <div className="my-2">
-                                    <StarsOnly rating={bm.wtRef.avgRating ? bm.wtRef.avgRating : 0}></StarsOnly>
+                                    {/* <StarsOnly rating={bm.wtRef.avgRating ? bm.wtRef.avgRating : 0}></StarsOnly> */}
                                     </div>
                                     <span className="text-sm flex gap-2 text-default-500 flex-wrap mt-2">
-                                        {bm.wtRef.genres && bm.wtRef.genres.map((genre:any) => {
-                                            return (
-                                                <span key={`${bm._id}-${genre._id}`} className="text-xs sm:text-sm flex">
-                                                    {genre.name}
-                                                </span>
-                                            )
-                                        })}
+                                        {bm.genres}
 
                                     </span>
                                 
@@ -80,7 +93,7 @@ export default function BookmarkListLocal () {
             </>
         : null
 
-        } */}
+        }
         
         </>
     )   
